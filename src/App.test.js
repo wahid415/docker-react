07bd2b5a11f2ae6f6f1e228bf141edee5426c3ed
wahid <mdwahidali122@gmail.com>
2025-05-02 00:07:26 +0530

    minor change for tests

--- src/App.test.js
+++ src/App.test.js
@@ -9,5 +9,5 @@ test("renders learn react link", () => {
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).not.toBeInTheDocument();
 });
